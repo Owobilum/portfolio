e6fa43969e651e80df470fbc76c5cd4dd4efcdb0
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-03-03 11:31:47 +0100

    build live project component

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,8 +1,9 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Grid } from '@mui/material'
 
 import PortfolioProject from '../PortfolioProject'
 import SectionHead from '../SectionHead'
-import { porfolioProjectData } from '../../utility/data/portfolioProjectData'
+import { porfolioProjectData, liveProjectData } from '../../utility/data/projectData'
+import LiveProject from '../LiveProject'
 
 const Projects = () => {
     return (
@@ -16,6 +17,36 @@ const Projects = () => {
                     <PortfolioProject key={project.title} project={project} />
                 ))
             }
+            <Typography component="p" sx={{ my: 5, color: 'text.secondary' }}>
+                Live Projects
+            </Typography>
+            <Grid container spacing={2}>
+                {
+                    liveProjectData && liveProjectData.map((project) => (
+                        <Grid item xs={12} md={4} key={project.title}
+                            sx={{
+                                transition: 'transform 0.25s ease-in-out',
+                                ":hover": {
+                                    color: 'text.secondary',
+                                    transform: 'translateY(-8px)',
+                                }
+                            }}
+                        >
+                            <Box component="a" href={project.site} target="_blank"
+                                sx={{
+                                    textDecoration: 'none',
+                                    color: 'text.grey',
+                                    ":hover": {
+                                        color: 'text.secondary'
+                                    }
+                                }}
+                            >
+                                <LiveProject project={project} />
+                            </Box>
+                        </Grid>
+                    ))
+                }
+            </Grid>
         </Box>
     )
 }
---
+++ src/utility/data/projectData.js
@@ -0,0 +1,33 @@
+import kioskImg from "../../images/kiosk.png"
+
+export const porfolioProjectData = [
+    {
+        img: kioskImg,
+        title: 'Kiosk E-Market',
+        description: 'An Intuitive E-commerce website with Paystack payments enabled',
+        stack: ['Next Js', 'Redux', 'Material UI', 'Firebase'],
+        github: 'https://github.com/Owobilum/kiosk',
+        site: 'https://kiosk-hazel.vercel.app/'
+    }
+]
+
+export const liveProjectData = [
+    {
+        title: 'iRecharge Web v2',
+        description: 'A bill payment and data management system for selling and purchasing electricity, airtime, data and TV subscriptions.',
+        stack: ['Next JS', 'Redux', 'Material UI'],
+        site: 'https://www.irecharge.ng/'
+    },
+    {
+        title: 'Accelerate',
+        description: 'A payment collection solution for merchants.',
+        stack: ['React JS', 'Redux', 'Material UI'],
+        site: 'https://accelerate-web.netlify.app/home'
+    },
+    {
+        title: 'Adara Dashboard',
+        description: 'A system for managing beneficiary data and product inventory of an NGO.',
+        stack: ['React JS', 'Redux', 'Reactstrap'],
+        site: 'http://tcp.adarafoundation.org/'
+    }
+]
